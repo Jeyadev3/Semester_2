f46c7f9076ab71781940e5e128f123f527d883a7
Jeyadev <jeyadev03@gmail.com>
2026-03-05 23:24:19 +0530

    Add files via upload

---
+++ Q2_Sum_of_10_Natural_Numbers.java
@@ -0,0 +1,9 @@
+
+public class Q2_Sum_of_10_Natural_Numbers {
+	public static void main(String[] args) {
+		int sum =0;
+		for(int i =1;i<11;i++) {
+			sum+=i;	}
+		System.out.println("The sum of first 10 natural numbers is : "+sum);
+	}
+}
