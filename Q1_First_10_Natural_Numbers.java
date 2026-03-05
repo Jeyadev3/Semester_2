ad0023e624b88e81a2cd323246914476da247802
Jeyadev <jeyadev03@gmail.com>
2026-03-05 23:23:59 +0530

    Add files via upload

---
+++ Q1_First_10_Natural_Numbers.java
@@ -0,0 +1,7 @@
+public class Q1_First_10_Natural_Numbers {
+        public static void main(String[] args) {
+            for(int i =0;i<=10;i++){
+                System.out.println(i);
+            }
+        }
+}
