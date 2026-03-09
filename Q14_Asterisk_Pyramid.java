3cf260030f15e3ef9ab13dcd7b98448ea79e270f
Jeyadev <jeyadev03@gmail.com>
2026-03-09 23:51:20 +0530

    Add files via upload

---
+++ Q14_Asterisk_Pyramid.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Q14_Asterisk_Pyramid {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter height: ");
+            int n = sc.nextInt();
+            int num = 1;
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n - i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*" + " ");
+                    num++;
+                }
+                System.out.println();
+            }
+            sc.close();
+        }
+    }
+
