5083f5153a32f11f5926caa2bc4dcf42924920c5
Jeyadev <jeyadev03@gmail.com>
2026-06-29 18:54:39 +0530

    Add files via upload

---
+++ Citizen.java
@@ -0,0 +1,16 @@
+package Moni;
+
+public class Citizen {
+	int aadhaar;
+	float rice;
+	float sugar;
+	float dhal;
+	
+	Citizen(int aadhaar,float rice,float sugar,float dhal){
+		this.aadhaar=aadhaar;
+		this.rice=rice;
+		this.sugar=sugar;
+		this.dhal=dhal;
+	}
+
+}
